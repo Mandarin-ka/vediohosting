3f2152f2c3e0f910c0732c7ec848a31023c607e0
Evgeniy <jdev03sgo@gmail.com>
2024-04-27 01:44:14 +0300

    feat: create API function

---
+++ src/API/getIMDbMovies.ts
@@ -0,0 +1,19 @@
+export const getIMDbMovies = async () => {
+  const url = 'https://imdb-top-100-movies.p.rapidapi.com/';
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '75ed199190msh0ecc099c1925197p16a28bjsn74078e1cd3f5', //TODO: Убрать в .env
+      'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com',
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response;
+    return result;
+  } catch (error: unknown) {
+    console.error((error as Error).message);
+  }
+};
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Page from '@pages/Page';
 import Page3 from '@utils/Page3';
 
 import * as styles from './App.module.scss';
+import { getIMDbMovies } from '@/API/getIMDbMovies';
 
 function App() {
+  const [movies, setMovies] = useState<any>();
+
+  useEffect(() => {
+    getIMDbMovies().then(async (data) =>
+      setMovies(JSON.parse(await data.text()))
+    );
+  }, []);
+
+  console.log(movies);
+  movies;
   return (
     <div className={styles.block}>
       vivivi
