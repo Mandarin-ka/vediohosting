a0edbb30e35fd6c2c0de7de3939d31b99774fcf3
Evgeniy <jdev03sgo@gmail.com>
2024-05-11 12:50:44 +0300

    refactor: footer icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import MainPage from '@pages/MainPage';
-import Footer from './components/Footer/Footer';
 
 import '@/styles/reset.scss';
 import '@/styles/vars.scss';
 
 function App() {
-  return <Footer />;
+  return <MainPage />;
 }
 
 export default App;
